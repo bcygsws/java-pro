9cab3867e9eeffb3a5badc2837e0f43043f21226
bcygsws <bcygswspring168@gmail.com>
2023-02-10 19:38:57 +0800

    :sparkles: 静态方法和实例方法

--- java-test/src/com/pro/test/Test.java
+++ java-test/src/com/pro/test/Test.java
@@ -4,12 +4,12 @@ package com.pro.test;
 // 注意2：静态块不管顺序如何，都会在main()方法之前执行
 class Father {
     void print() {
-        System.out.println("Father类");
+        System.out.println("Father类中print方法");
     }
 }
 
 public class Test extends Father {
-    //    静态块在类被加载时调用
+//      静态块在类被加载时调用
 //    static {
 //        System.out.println("静态块不论书写位置如何都会在入口函数之前执行");
 //    }
@@ -34,7 +34,5 @@ public class Test extends Father {
  * 3.static方法只能访问static方法（和static成员），不能访问非static方法，但非static方法可以访问静态方法
  * 4.static方法是属于整个类的，它在内存中随着类的定义而被分配和装载，而非static定义的方法是属于某个对象的方法
  * 在这个对象创建时，在对象的内存中拥有这个方法的专用代码段
- *
- *
  *
  * */
