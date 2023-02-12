7450205a9f416614f62ae37a3b7f5e13c41d7a05
bcygsws <bcygswspring168@gmail.com>
2023-02-12 21:53:48 +0800

    :memo: Java三大特性之封装

--- java-character/src/com/company/cn/Application.java
+++ java-character/src/com/company/cn/Application.java
@@ -7,7 +7,7 @@ public class Application {
         Student s = new Student();
         // 1.报错：name在com.company.cn.Student中是private访问控制;name是Student类中的私有属性；私有属性不能够在当前类之外的
         // 地方直接调用
-//         System.out.println(s.name);
+        // System.out.println(s.name);
         // 为此不能在Application类中直接使用实例s访问类成员
         // 2.在Student类中定义一个方法getName
         // getName方法访问控制符是public，任何类都可以访问，所以不会报错了
@@ -25,9 +25,13 @@ public class Application {
 }
 /**
  * @ Java三大特性：封装
+ * 参考文档：https://blog.csdn.net/weixin_47556601/article/details/122521852
  * 体会封装的好处
  * 1.提高程序的安全性，保护数据
  * 2.隐藏代码的实现细节 【setX系列中处理传入的参数的逻辑，对Application是隐藏的】
  * 3.统一接口
  * 4.提高代码的可维护性
+ * 代码的 高内聚，低耦合
+ * 高内聚：是代码内部的实现细节主动控制
+ * 低耦合：向外暴露少量的方法
  */
