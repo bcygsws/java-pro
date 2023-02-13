10c57ac325559ea79bb7fec01f39708733ebe931
bcygsws <bcygswspring168@gmail.com>
2023-02-13 19:30:07 +0800

    :ambulance: Java项目的创建过程

---
+++ java-inheritance/src/com/my/test/Inheritance.java
@@ -0,0 +1,8 @@
+package com.my.test;
+// 测试Java的基本特征；继承
+public class Inheritance {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
---
+++ java-polymorphism/src/com/company/cx/Polymorphism.java
@@ -0,0 +1,10 @@
+package com.company.cx;
+
+// 多态代码的演示
+public class Polymorphism {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello World!");
+    }
+}
